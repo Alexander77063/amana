642c27469771d9971f4ceea58da97938fda3bc7b
Alex Adegbola <alexanderadegbola@gmail.com>
2026-05-03 17:23:10 +0100

    feat(wallet): module barrel

---
+++ apps/backend/src/modules/wallet/index.ts
@@ -0,0 +1,37 @@
+export {
+  ledgerAccountsRepo,
+  type LedgerAccountKind,
+  type LedgerAccountRow,
+  type NewLedgerAccount,
+  type NormalSide,
+} from './ledger-accounts.repo';
+
+export {
+  masterWalletsRepo,
+  type MasterWalletRow,
+  type ProvisionedMasterWallet,
+  type ProvisionInput,
+} from './master-wallets.repo';
+
+export {
+  subWalletsRepo,
+  type ProvisionedSubWallet,
+  type ProvisionSubInput,
+  type SubWalletRow,
+} from './sub-wallets.repo';
+
+export {
+  transactionsRepo,
+  type NewTransaction,
+  type TransactionRow,
+  type TxnKind,
+  type TxnStatus,
+} from './transactions.repo';
+
+export {
+  postingsRepo,
+  type NewPosting,
+  type PostingRow,
+} from './postings.repo';
+
+export { ledgerService, type DoubleEntryLeg } from './ledger.service';
